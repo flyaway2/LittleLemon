194cf89581ea67d6b3723778ba66c23ace5d05bc
flyaway2 <h.amine.dz@hotmail.fr>
2023-05-16 21:04:11 +0100

    added rest api for Menu model to list items and add new ones

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Booking,Menu
+from .serializers import MenuSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.db import IntegrityError
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 def index(request):
     return render(request,"index.html",{})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset=Menu.objects.all()
+    #permission_classes=[IsAuthenticated]
+    serializer_class=MenuSerializer
+    # def create(self, request):
+    #     serializer=MenuSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         try:
+    #             serializer.save()
+    #             return Response(serializer,status=status.HTTP_200_OK)
+    #         except IntegrityError:        
+    #             return Response({"msg":"item already exist"},status=status.HTTP_400_BAD_REQUEST)
+    #     else:
+    #         return Response({"msg":"invalid data"},status=status.HTTP_400_BAD_REQUEST)
+            
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+
+            
+        
+    
