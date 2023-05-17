fbd7f432db49087a7cff0fca2e671155390e95f4
flyaway2 <h.amine.dz@hotmail.fr>
2023-05-17 21:01:15 +0100

    added authentication to table booking api

--- workspace/littlelemon/littlelemon/urls.py
+++ workspace/littlelemon/littlelemon/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from restaurant import views
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -27,5 +28,6 @@ urlpatterns = [
     path('restaurant/',include('restaurant.urls')),
     path('restaurant/booking/',include(router.urls)),
     path('auth/',include('djoser.urls')),
-    path('auth/',include('djoser.urls.authtoken'))
+    path('auth/',include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token)
 ]
